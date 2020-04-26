61e225d98a256030ed69b8758f54a776536788e0
wling <961759850@qq.com>
2020-04-26 14:11:11 +0800

    完成句子补全功能,基本能用,但是补全比较蠢

--- src/api/word-api.js
+++ src/api/word-api.js
@@ -7,21 +7,22 @@ module.exports.search_word = function (word) {
 
         let tmp = word.split(' ');
         let target = tmp[tmp.length - 1];
-        //查询单词, 如果最后一个单词是一个完整的单词说明应该推算下一个可能的单词
-        // let sentenceReg = new RegExp(target);
-        // if(dictMap.has(target)) {
-            // dictMap.forEach((value, key) => {
-                // // 通过正则表达式找到出现频率最高的句子中的下一个单词
-                // if(sentenceReg.test(target)) {
-                    // let tmp = key.split(' ');
-                    // if(tmp.length - 1 > tmp.indexOf(target)) {
-                        
-                    // }
-                // }
-            // })
-        // }
-        //补全单词, 如果最后一个单词不是一个完整的单词说明应该补全单词
         if(target == "") resolve("");
+        //查询单词, 如果最后一个单词是一个完整的单词说明应该推算下一个可能的单词
+        if(dictMap.has(target)) {
+            dictMap.forEach((value, key) => {
+                let str = String(key);
+                let tmp = str.split(" ");
+                console.log(tmp);
+                let index = tmp.indexOf(target);
+                if(tmp.length > 1&& index < tmp.length - 1 && index >= 0) {
+                    resolve(" " + tmp[index + 1]);
+                    return;
+                }
+            })
+            resolve("");
+        }
+        //补全单词, 如果最后一个单词不是一个完整的单词说明应该补全单词
         let res = "", freq = 0;
         let re = new RegExp('^' + target + '+');
         dictMap.forEach((value, key) => {
@@ -36,10 +37,10 @@ module.exports.search_word = function (word) {
 }
 
 module.exports.addFrequency = function (word) {
-    console.log(word)
     word.forEach((ele) => {
         dictMap.set(ele, (dictMap.get(ele) + 1) || 1);
         console.log(dictMap.get(ele));
     })
-    dictMap.set(word, (dictMap.get(word) + 1) || 1);
+    //把整个句子放到map里面
+    dictMap.set(word.join(" "), (dictMap.get(word) + 1) || 1);
 }
